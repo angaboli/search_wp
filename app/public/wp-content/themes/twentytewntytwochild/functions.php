5a513329f723242aef561e582a783ad7de51b25f
Ngabonziza Aimé <nzizaba@gmail.com>
2022-03-04 07:53:02 +0100

    article

--- app/public/wp-content/themes/twentytewntytwochild/functions.php
+++ app/public/wp-content/themes/twentytewntytwochild/functions.php
@@ -15,6 +15,7 @@ function tagwalk_title_separator (){
 
 }
 
+
 add_action('after_setup_theme', 'tagwalk_supports');
 add_action('wp_enqueue_scripts', 'tagwalk_register_assets');
 add_filter( 'document_title_separator', 'tagwalk_title_separator' );
--- app/public/wp-content/themes/twentytewntytwochild/index.php
+++ app/public/wp-content/themes/twentytewntytwochild/index.php
@@ -1,7 +1,17 @@
 
-<?php get_header( ) ?>
+    <?php get_header( ) ?>
 
-This my theme
+    <?php if (have_posts()): ?>
+        <ul>
+            <?php while(have_posts()): the_post();  ?>
+                <li><?php the_title() ?></li>
+            <?php endwhile ?>
 
-<?php get_footer( ) ?>
+        </ul>
+    <?php else: ?>
+        <h1>Pas d'articles</h1>
+    <?php endif; ?>
+
+
+    <?php get_footer( ) ?>
 
